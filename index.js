3c23bc300d2859e93a220b03409cffcfb93d3510
sikhauli <sikhaulivl@gmail.com>
2022-07-14 10:13:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// var tag_id = document.getElementsByClassName('title');
+// tag_id.innerHTML = 'HTML string';
+
+
+
+// const getElement = document.getElementsByClassName('title');
+// const NewElement = document.createElement('h2');
+// NewElement.appendChild(document.createTextNode('This is a demo string'));
+
+
+const getElement = document.getElementsByClassName('title');
+getElement.innerHTML = 'This is a demo string';
+
